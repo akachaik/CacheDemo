8709cd319b97d3e34ae1315c9bdff8296646c4bf
akachai <akachai@mail.com>
2023-11-06 19:23:38 +0700

    Add project files.

---
+++ CacheDemo/Application/Caching/ICacheService.cs
@@ -0,0 +1,16 @@
+﻿namespace CacheDemo.Application.Caching;
+
+public interface ICacheService
+{
+    Task<T?> GetAsync<T>(string cacheKey, CancellationToken cancellationToken = default)
+        where T : class;
+
+    Task<T> GetAsync<T>(string cacheKey, Func<Task<T>> factory, CancellationToken cancellationToken = default)
+        where T : class;
+    Task SetAsync<T>(string cacheKey, T value, CancellationToken cancellationToken = default)
+        where T : class;
+
+    Task RemoveAsync(string cacheKey, CancellationToken cancellationToken = default);
+
+    Task RemoveByPrefixAsync(string prefixKey, CancellationToken cancellationToken = default);
+}
---
+++ CacheDemo/Infrastructure/Caching/CachingService.cs
@@ -0,0 +1,88 @@
+﻿using CacheDemo.Application.Caching;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Collections.Concurrent;
+using System.Text.Json;
+
+namespace CacheDemo.Infrastructure.Caching;
+
+public class CachingService : ICacheService
+{
+    private static readonly ConcurrentDictionary<string, bool> CachedKeys = new();
+    
+    private readonly IDistributedCache _distributedCache;
+
+    public CachingService(IDistributedCache distributedCache)
+    {
+        _distributedCache = distributedCache;
+    }
+
+    public async Task<T?> GetAsync<T>(string cacheKey, CancellationToken cancellationToken = default) where T : class
+    {
+        string? cachedValue = await _distributedCache.GetStringAsync(cacheKey, cancellationToken);
+
+        if (cachedValue is null)
+        {
+            return null;
+        }
+
+
+        T? value = JsonSerializer.Deserialize<T>(cachedValue);
+
+        return value;
+    }
+
+    public async Task<T> GetAsync<T>(string cacheKey, Func<Task<T>> factory, CancellationToken cancellationToken = default) where T : class
+    {
+        T? cachedValue = await GetAsync<T>(cacheKey, cancellationToken);
+
+        if (cachedValue is not null)
+        {
+            return cachedValue;
+        }
+
+        cachedValue = await factory();
+
+        await SetAsync(cacheKey, cachedValue, cancellationToken);
+
+        return cachedValue;
+
+    }
+
+    public async Task RemoveAsync(string cacheKey, CancellationToken cancellationToken = default)
+    {
+        await _distributedCache.RemoveAsync(cacheKey, cancellationToken);
+
+        CachedKeys.TryRemove(cacheKey, out var _);
+
+    }
+
+    public async Task RemoveByPrefixAsync(string prefixKey, CancellationToken cancellationToken = default)
+    {
+        //foreach (var key in CachedKeys.Keys)
+        //{
+        //    if (key.StartsWith(prefixKey))
+        //    {
+        //        await _distributedCache.RemoveAsync(key, cancellationToken);
+
+        //    }
+        //}
+
+        var removeTasks = CachedKeys
+            .Keys
+            .Where(k => k.StartsWith(prefixKey))
+            .Select(k => _distributedCache.RemoveAsync(k, cancellationToken));
+
+        await Task.WhenAll(removeTasks);
+
+
+    }
+
+    public async Task SetAsync<T>(string cacheKey, T value, CancellationToken cancellationToken = default) where T : class
+    {
+        string cacheValue = JsonSerializer.Serialize(value);
+
+        await _distributedCache.SetStringAsync(cacheKey, cacheValue, cancellationToken);
+
+        CachedKeys.TryAdd(cacheKey, false);
+    }
+}
